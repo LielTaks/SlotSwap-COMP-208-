696d32b611841574bb66a27bfe18389f9c6ca658
LielTaks <165488960+LielTaks@users.noreply.github.com>
2024-04-02 17:05:13 +0200

    Delete dashboard.php

--- dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php session_start(); 
-if($_SESSION["Role"]=='Admin'){
- // header('Location: index.php');
-} else {
- header('Location: index.php');
-}
-// include('templates/head-dash.php'); 
-include('search.php'); 
-?>
